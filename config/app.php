a07c4eb69528364450ceab6324388063091747aa
誓言之印 <hhzgq@163.com>
2017-05-14 03:04:43 +0800

    Add laravel-debugbar

--- config/app.php
+++ config/app.php
@@ -177,7 +177,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+		Barryvdh\Debugbar\ServiceProvider::class
     ],
 
     /*
@@ -227,6 +228,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+		'Debugbar' => Barryvdh\Debugbar\Facade::class
     ],
 
 ];
