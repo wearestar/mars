bbf51e63879fa44a506f1b5278dc75c69ece7c9d
Gilbertat <shiyue45457@gmail.com>
2017-05-16 12:23:24 +0800

    add 3 controllers ‘SessionsController’, ‘UsersController’, ‘StaticPagesController’,add Models folders on the app, move the User.php file to Models folders, add 3 routes, about home, signUp, login

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SessionsController extends Controller
+{
+    // 用户登录操作
+    public function create()
+    {
+        return '登录表单';
+    }
+}
---
+++ app/Http/Controllers/StaticPagesController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StaticPagesController extends Controller
+{
+    // 首页展示页面
+    public function home()
+    {
+        return '静态页面';
+    }
+}
