843010fd90b2e9960f7d380b7c4f1e7a709fec17
Gilbertat <shiyue45457@gmail.com>
2017-05-29 09:20:32 +0800

    add font-awesome compoment

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,6 @@
 /*静态页面 例如header footer, 静态Body*/
 Route::get('/', 'StaticPagesController@home')->name('home');
 
-/*用户相关操作 注册， 修改密码， 用户激活等*/
-Route::get('signup','UsersController@create')->name('signup');
-
-/*用户状态操作 登录， 退出等*/
-Route::get('login', 'SessionsController@create')->name('login');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
