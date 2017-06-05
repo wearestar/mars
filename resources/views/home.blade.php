acf8d443b0ccb6ffc567befb45ea8e879e285549
Berdon Lee <huasheng28@qq.com>
2017-06-05 17:02:53 +0800

    update home and nav , change the app_name and database name of  .env file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.default')
-@section('title','首页')
+
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Welcome to Mars!</div>
                 <div class="panel-body">
-                    Welcome to Mars!
+                    <p class="text-center">Develop Your Journey on Mars Planet.</p>
+                    <p class="text-center">There is something amazing you can wirte about our Mars Blog.</p>
                 </div>
             </div>
         </div>
     </div>
-    
 </div>
 @endsection
--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -11,7 +11,7 @@
             </button>
 
             <!-- Branding Image -->
-            <a class="navbar-brand" href="{{ url('/') }}">
+            <a class="navbar-brand" href="{{route('home')}}">
                 {{ config('app.name', 'Mars') }}
             </a>
         </div>
@@ -19,9 +19,15 @@
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
             <!-- Left Side Of Navbar -->
             <ul class="nav navbar-nav">
-                &nbsp;
+                <li class="active"><a href="{{route('home')}}">主页<span class="sr-only">(current)</span></a></li>
+                <li><a href="#">发现</a></li>
             </ul>
-
+            <form class="navbar-form navbar-left">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Search">
+                </div>
+                <button type="submit" class="btn btn-default">搜索</button>
+            </form>
             <!-- Right Side Of Navbar -->
             <ul class="nav navbar-nav navbar-right">
                 <!-- Authentication Links -->
@@ -29,6 +35,16 @@
                     <li><a href="{{ route('register') }}">注册</a></li>
                     <li><a href="{{ route('login') }}">登录</a></li>
                 @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">消息<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li><a href="#">Something else here</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">Separated link</a></li>
+                        </ul>
+                    </li>
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                             {{ Auth::user()->name }} <span class="caret"></span>
